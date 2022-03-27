cf178126d0233bcda22995700ab93f71ef4906f3
Iliasoft <98062362+Iliasoft@users.noreply.github.com>
2022-03-27 04:30:43 +0300

    HW4 Problem 2 - Vending Machine class that supports overloading of +, -, less then and greater than methods.

--- HW4/Post.py
+++
@@ -1,8 +0,0 @@
-class Post():
-
-    def __init__(self, id, header, body, timestamp):
-        self.id = id
-        self.header = header
-        self.body = body
-        self.timestamp = timestamp
-
