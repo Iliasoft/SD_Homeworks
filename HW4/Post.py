76afda8a58cd6b38ffaa175ccba3b60a9b165231
Iliasoft <98062362+Iliasoft@users.noreply.github.com>
2022-03-27 04:14:37 +0300

    HW1 Problem 1 - Flash Blog App (initial version)

---
+++ HW4/Post.py
@@ -0,0 +1,8 @@
+class Post():
+
+    def __init__(self, id, header, body, timestamp):
+        self.id = id
+        self.header = header
+        self.body = body
+        self.timestamp = timestamp
+
